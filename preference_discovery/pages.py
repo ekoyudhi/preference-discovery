4394cefa9bed290817b0f272ab2aee2e1174bb09
Anindya Arman Putri <93259436+anindyaarman@users.noreply.github.com>
2022-04-01 11:27:15 +0700

    Update pages.py

--- preference_discovery/pages.py
+++ preference_discovery/pages.py
@@ -438,10 +438,6 @@ class No6EndResult(Page):
             'payoff_all_rupiah' : payoff_all * 500 + 5000,
         }
     
-class No6End(Page):
-   
-    def is_displayed(self):
-        return self.round_number == 1
 
 page_sequence = [No1Introduction,
                  No2Instructions1,
@@ -463,5 +459,5 @@ page_sequence = [No1Introduction,
                  No5Result3,
                  No6EndQuestionnaire,
                  No6EndResult,
-                 No6End]
+                 ]
 
